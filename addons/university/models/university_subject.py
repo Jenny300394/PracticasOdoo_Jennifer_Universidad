1007b92a1dcbab70ea4f5f65aa0762750a655fc1
Jennifer <jennifercaballero3003@gmail.com>
2026-02-26 14:08:41 +0100

    Subject corregida completa

--- addons/university/models/university_subject.py
+++ addons/university/models/university_subject.py
@@ -20,13 +20,15 @@ class Subject(models.Model):
     # Contadores para Smart Buttons
     enrollment_count = fields.Integer(string="Enrollments", compute="_compute_counts")
     student_count = fields.Integer(string="Students", compute="_compute_counts")
+    professor_count = fields.Integer(string="Professors Count", compute="_compute_counts")
 
-    @api.depends('enrollment_ids')
+    @api.depends('enrollment_ids', 'professor_ids')
     def _compute_counts(self):
         for record in self:
             record.enrollment_count = len(record.enrollment_ids)
-            # Extraemos los alumnos únicos de las matrículas
+            # Alumnos únicos: usamos set() o mapped().ids para contar IDs reales
             record.student_count = len(record.enrollment_ids.mapped('student_id'))
+            record.professor_count = len(record.professor_ids)
 
     # Acción del botón de matrículas
     def action_view_enrollments(self):
@@ -49,5 +51,17 @@ class Subject(models.Model):
             "name": "Students",
             "res_model": "university.student",
             "view_mode": "list,kanban,form",
-            "domain": [("id", "in", student_ids)],
+            # Filtramos por los IDs de estudiantes encontrados o pasamos 0 para lista vacía
+            "domain": [("id", "in", student_ids or [0])],
+        }
+
+    # Acción del botón de profesores (Por si lo usas en el XML)
+    def action_view_professors(self):
+        self.ensure_one()
+        return {
+            "type": "ir.actions.act_window",
+            "name": "Professors",
+            "res_model": "university.professor",
+            "view_mode": "list,form",
+            "domain": [("id", "in", self.professor_ids.ids or [0])],
         }
